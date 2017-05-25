cf0a60c0dc874e2a4f273d7dc8ac01c98c8f4de0
hshami17 <hasanshami2013@gmail.com>
2017-05-24 23:11:15 -0400

    Changed setup script a bit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,11 @@
 # Get web server packages
 sudo pip install Flask
 sudo pip install Flask-SocketIO
-sudo pip install eventlet
+sudo apt-get install python-dev
+sudo pip install greenlet
 
 sudo apt-get install figlet
 sudo apt-get install screen
 
+# Remove standard java
+sudo apt-get remove --purge oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre
