976245db4679a2677e5be3e6b188f610d009ba71
hshami17 <hasanshami2013@gmail.com>
2018-02-05 00:39:11 -0500

    Adding fade to headlines in news module when they scroll out of view

--- src/main/java/module/news/NewsAPIController.java
+++ src/main/java/module/news/NewsAPIController.java
@@ -9,14 +9,21 @@ import api_calls.NewsAPI;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
@@ -45,6 +52,8 @@ public class NewsAPIController implements Initializable, ModuleControl,
     private double scrollAmount = 0.0;
     private boolean scrollDown = true;
     
+    private List<Headline> currentHeadlines = new ArrayList<>();
+
     /**
      * Initializes the controller class.
      */
@@ -60,8 +69,49 @@ public class NewsAPIController implements Initializable, ModuleControl,
         // Auto scroll
         configureAutoScroll();
         
+        configureScrollFade();
+        
         PCS.INST.addPropertyChangeListener(PCM.NEWS_UPDATE, this);
-    }    
+    }  
+    
+    private final double FADE_THRESHOLD = 90;
+    private void configureScrollFade() {
+        newsModule.vvalueProperty().addListener(new ChangeListener<Number>() 
+            {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) 
+                {
+                    currentHeadlines.forEach((l) -> {
+                        Bounds bounds = getVisibleBounds(l);
+                        double minY = bounds.getMinY();
+                        double maxY = bounds.getMaxY();
+
+                        if(Math.abs(minY) < FADE_THRESHOLD ) {
+                            l.setOpacity(1 - (FADE_THRESHOLD - Math.abs(minY)) / FADE_THRESHOLD);
+                        }
+                        else if(Math.abs(maxY) < FADE_THRESHOLD) {
+                            l.setOpacity(1 - (FADE_THRESHOLD - Math.abs(maxY)) / FADE_THRESHOLD);
+                        }
+                        else {
+                            l.setOpacity(1);
+                        }
+                    });
+                }
+            });
+    }
+
+    private Bounds getVisibleBounds(Node aNode) {
+        // If node not visible, return empty bounds
+        if(!aNode.isVisible()) return new BoundingBox(0,0,-1,-1);
+
+        // If node has clip, return clip bounds in node coords
+        if(aNode.getClip()!=null) return aNode.getClip().getBoundsInParent();
+
+        // If node has parent, get parent visible bounds in node coords
+        Bounds bounds = aNode.getParent()!=null? getVisibleBounds(aNode.getParent()) : null;
+        if(bounds!=null && !bounds.isEmpty()) bounds = aNode.parentToLocal(bounds);
+        return bounds;
+    }
     
     private void configureAutoScroll(){        
         // Create auto scroll animation
@@ -127,8 +177,11 @@ public class NewsAPIController implements Initializable, ModuleControl,
     public void propertyChange(PropertyChangeEvent evt) {
         Platform.runLater(() -> {
            newsList.getChildren().clear();
+           currentHeadlines.clear();
            for (String headline : newsAPIModel.getHeadlines()){
-               newsList.getChildren().add(new Headline(headline));
+               Headline h = new Headline(headline, newsModule);
+               newsList.getChildren().add(h);
+               currentHeadlines.add(h);
            }
        });
     }    
